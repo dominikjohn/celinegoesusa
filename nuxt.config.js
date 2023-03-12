4898dd39debb23761af0c73a2d2b1243531480aa
dominikjohn <mail@dominikjohn.com>
2023-03-12 18:55:23 +0100

    c

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Neuland Neuseeland',
+    title: 'Céline goes USA',
     htmlAttrs: {
       lang: 'de'
     },
